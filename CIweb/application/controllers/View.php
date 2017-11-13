3b0fb346b79023e22424615d3b2374e9bead31a3
Angga <angga.kresnabayu@gmail.com>
2017-11-13 22:26:12 +0700

    CI

---
+++ CIweb/application/controllers/View.php
@@ -0,0 +1,6 @@
+<?php
+class View extends CI_Controller{
+	public function index(){
+		$this->load->view('view');
+	}
+}
---
+++ CIweb/application/views/list1.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Objek Wisata Bandung</title>
+	<link href="https://fonts.googleapis.com/css?family=Sacramento|Tangerine" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="Navbar.css">
+	<link rel="stylesheet" type="text/css" href="Grid.css">
+</head>
+<body background="Gambar/cloud.jpg">
+<div class="navbar">
+	<nav>
+		<ul>
+			<li><a href="Page 1 (Home).html">Home</a></li>
+			<li><a href="Page 1.5 (View).html">View</a></li>
+			<li><a href="Page 2 (List).html">List</a>
+			<ul>
+				<li><a href="Page 3 (List1).html">Wisata Budaya</a></li>
+				<li><a href="Page 4 (List2).html">Wisata Alam</a></li>
+			</ul>
+			</li>
+			<li><a href="Page 5 (About Us).html">About Us</a></li>
+			<a href="https://facebook.com/"><img src="Gambar\facebook.png" width=2.5%></a>
+			<a href="https://twitter.com/"><img src="Gambar\twitter2.png" width=2.5%></a>
+			<a href="https://instagram.com/"><img src="Gambar\instagram1.png" width=2.5%></a>
+		</ul>
+	</nav>
+</div>
+<!-- !PAGE CONTENT! -->
+<div class="p2-content" style="max-width:2000px">
+<!-- Header -->
+	<div class="p2-opacity">
+	<span class="p2-button p2-xxlarge p2-white p2-right" onclick="p2_open()"><i class="fa fa-bars"></i></span> 
+	<div class="p2-clear"></div>
+	<header class="p2-center p2-margin-bottom">
+		<h1><b>Wisata Budaya Bandung</b></h1>
+		<p><b>Menapaki kearifan lokal yang terpancar dari Bandung</b></p>
+	</header>
+</div>
+<!-- Photo Grid -->
+<div class="p2-row" id="myGrid" style="margin-bottom:120px">
+  <div class="p2-one">
+  	<a href="Page 3.1.html">
+  	<img src="Gambar/Braga.jpg" style="width:100%">
+  </a>
+  	<a href="Page 3.2.html">
+    <img src="Gambar/Goa.jpg" style="width:100%">
+   </a>
+   <a href="Page 3.1.html">
+   	<img src="Gambar/Jaarbeurs.jpg" style="width:100%">
+   </a>
+    <a href="Page 3.2.html">
+   	<img src="Gambar/Udjo.jpg" style="width:100%">
+   </a>
+    <a href="Page 3.1.html">
+   	<img src="Gambar/GedungMerdeka.jpg" style="width:100%">
+   </a>
+  </div>
+ <div class="p2-two">
+ 	<a href="Page 3.2.html">
+ 	<img src="Gambar/TamanBudaya.jpg" style="width:100%">
+ 	</a>
+ 	<a href="Page 3.1.html">
+    <img src="Gambar/Rumentang.jpg" style="width:100%">
+	</a>
+	<a href="Page 3.2.html">
+   	<img src="Gambar/GedungSate.jpg" style="width:100%">
+   </a>
+   <a href="Page 3.1.html">
+   	<img src="Gambar/SriBaduga.jpg" style="width:100%">
+   	</a>
+   	<a href="Page 3.2.html">
+   	<img src="Gambar/Pos.jpg" style="width:100%">
+   </a>
+  </div>
+  <div class="p2-third">
+  	<a href="Page 3.1.html">
+  	<img src="Gambar/Mahmud.jpg" style="width:100%">
+  	</a>
+  	<a href="Page 3.2.html">
+    <img src="Gambar/Cireundeu2.jpg" style="width:100%">
+	</a>
+	<a href="Page 3.1.html">
+   	<img src="Gambar/Menggugat.jpg" style="width:100%">
+   </a>
+   <a href="Page 3.2.html">
+   	<img src="Gambar/MuseumBarli.jpg" style="width:100%">
+   </a>
+   <a href="Page 3.1.html">
+   	<img src="Gambar/KampungAdatCikondang.jpg" style="width:100%">
+	</a>
+  </div>
+</div>
+</body>
+</html>
